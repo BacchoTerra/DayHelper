e9ba4c9a9f0f8595ace72ba77e86b20bdccd4ae6
Bruno Baccho Terra <brunobacchot@hotmail.com>
2020-08-22 19:06:32 -0300

    Initial commit

---
+++ app/src/main/java/com/bacchoterra/dayhelper/MainActivity.java
@@ -0,0 +1,47 @@
+package com.bacchoterra.dayhelper;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Layout
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+
+        toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.activity_main_drawerLayout);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+}
