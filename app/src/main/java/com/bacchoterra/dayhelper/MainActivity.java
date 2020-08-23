bde43ea96e82c3a23f14cd3fc61d2db69c68838d
Bruno Baccho Terra <brunobacchot@hotmail.com>
2020-08-23 01:57:35 -0300

    Adicionado escolha de mood no layout principal.

--- app/src/main/java/com/bacchoterra/dayhelper/MainActivity.java
+++ app/src/main/java/com/bacchoterra/dayhelper/MainActivity.java
@@ -6,28 +6,50 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock;
 
 public class MainActivity extends AppCompatActivity {
 
     //Layout
+    private ViewGroup rootLayout;
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private CustomAnalogClock analogClock;
+
+    //Colors/Feelings
+    private int happy;
+    private int anger;
+    private int sad;
+    private int love;
+    private int fear;
+
+    private int currentColor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
     }
 
     private void init() {
         initViews();
         initDrawerLayout();
         initAnalogClock();
+        initColors();
 
     }
 
@@ -42,7 +64,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void initViews(){
+    private void initViews() {
+        rootLayout = findViewById(R.id.activity_main_rootLayout);
         toolbar = findViewById(R.id.activity_main_toolbar);
         drawerLayout = findViewById(R.id.activity_main_drawerLayout);
         analogClock = findViewById(R.id.activity_main_analogClock);
@@ -60,5 +83,62 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+    }
+
+    private void initColors() {
+
+        happy = getResources().getColor(R.color.happy);
+        sad = getResources().getColor(R.color.sad);
+        fear = getResources().getColor(R.color.fear);
+        love = getResources().getColor(R.color.love);
+        anger = getResources().getColor(R.color.anger);
+
+        Drawable background = rootLayout.getBackground();
+        if (background instanceof ColorDrawable)
+            currentColor = ((ColorDrawable) background).getColor();
+
+    }
+
+    private void changeBackgroundColor(final int colorTo) {
+        ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), currentColor, colorTo);
+        colorAnimation.setDuration(600); // milliseconds
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                rootLayout.setBackgroundColor((int) animator.getAnimatedValue());
+                toolbar.setBackgroundColor((int) animator.getAnimatedValue());
+                currentColor = colorTo;
+
+
+            }
+
+        });
+        colorAnimation.start();
+    }
+
+    public void fabChoice(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.activity_main_fabHappy:
+                changeBackgroundColor(happy);
+                break;
+            case R.id.activity_main_fabSad:
+                changeBackgroundColor(sad);
+                break;
+            case R.id.activity_main_fabFear:
+                changeBackgroundColor(fear);
+                break;
+            case R.id.activity_main_fabLove:
+                changeBackgroundColor(love);
+                break;
+            case R.id.activity_main_fabAnger:
+                changeBackgroundColor(anger);
+                break;
+
+        }
+
+
     }
 }
