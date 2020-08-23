8fff2c6c273e971f28882a2550adc27b23ab305f
Bruno Baccho Terra <brunobacchot@hotmail.com>
2020-08-23 02:54:27 -0300

    Adicionadas frases de hint para cada mood.

--- app/src/main/java/com/bacchoterra/dayhelper/MainActivity.java
+++ app/src/main/java/com/bacchoterra/dayhelper/MainActivity.java
@@ -13,8 +13,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Switch;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock;
@@ -26,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private CustomAnalogClock analogClock;
+    private TextView txtMoodDesc;
+    private EditText editFeeling;
 
     //Colors/Feelings
     private int happy;
@@ -68,7 +69,9 @@ public class MainActivity extends AppCompatActivity {
         rootLayout = findViewById(R.id.activity_main_rootLayout);
         toolbar = findViewById(R.id.activity_main_toolbar);
         drawerLayout = findViewById(R.id.activity_main_drawerLayout);
+        txtMoodDesc = findViewById(R.id.activity_main_txtMoodDesc);
         analogClock = findViewById(R.id.activity_main_analogClock);
+        editFeeling = findViewById(R.id.activity_main_editFeeling);
     }
 
     private void initAnalogClock() {
@@ -123,18 +126,28 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.activity_main_fabHappy:
                 changeBackgroundColor(happy);
+                txtMoodDesc.setText(R.string.we_are_glad_you_are_happy);
+                editFeeling.setHint(R.string.happiness_is_always_contagious_save_your_best_moments);
                 break;
             case R.id.activity_main_fabSad:
                 changeBackgroundColor(sad);
+                txtMoodDesc.setText(R.string.it_is_ok_to_be_sad);
+                editFeeling.setHint(R.string.sadness_is_sometimes_a_great_friend_we_can_learn_a_lot_from_it_why_blue_today);
                 break;
             case R.id.activity_main_fabFear:
                 changeBackgroundColor(fear);
+                txtMoodDesc.setText(R.string.fear_is_only_natural);
+                editFeeling.setHint(R.string.fear_gives_us_the_possibility_to_remember_that_we_are_courageous_what_s_going_on);
                 break;
             case R.id.activity_main_fabLove:
                 changeBackgroundColor(love);
+                txtMoodDesc.setText(R.string.love_is_what_we_all_need);
+                editFeeling.setHint(R.string.never_forget_value_current_things_they_are_the_best_what_do_you_value_today);
                 break;
             case R.id.activity_main_fabAnger:
                 changeBackgroundColor(anger);
+                txtMoodDesc.setText(R.string.dont_let_anger_make_your_decisions);
+                editFeeling.setHint(R.string.anger_is_momentary_i_guarantee_you_it_is_never_worth_it_what_is_bothering_you);
                 break;
 
         }
